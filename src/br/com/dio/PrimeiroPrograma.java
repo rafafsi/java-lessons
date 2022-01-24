18a79be5d497b08f723a5daa4cd6ee2b0ed655cf
rafafsi <rafaelafsinacio@gmail.com>
2022-01-24 15:33:39 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,8 +7,42 @@ public class PrimeiroPrograma {
        /* System.out.println("hello");*/
         Gato gato = new Gato();
 
+        Livro livro = new Livro("o santo e a porca", 54);
+        System.out.println(livro);
 
+    }
+}
 
+class Livro {
+    private String nome;
+    private Integer numPaginas;
 
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
