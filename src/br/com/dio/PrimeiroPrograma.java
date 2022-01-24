cc972363d708a13c9df9232d9b59f0c0654e7c5b
rafafsi <rafaelafsinacio@gmail.com>
2022-01-24 11:46:47 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+       /* System.out.println("hello");*/
+        Gato gato = new Gato();
+
+
+
+
+    }
+}
